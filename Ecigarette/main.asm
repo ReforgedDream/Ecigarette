1eb4864d2551b6e7d686443280709cd9a644dd11
ReforgedDream <DGH&g3ddk9l>
2016-07-20 21:46:33 +0600

    Transfer to ATMega64A (started)
    Porting on atmega64a, half of work done. Untested.

--- Ecigarette/main.asm
+++ Ecigarette/main.asm
@@ -1,26 +1,75 @@
 ; Created: 25.05.2016 14:12:47
 
-.include "tn13adef.inc"
+;.include "tn13adef.inc"
+.include "m64adef.inc"
 
-.device attiny13a
+;.device attiny13a
+.device atmega64a
 
 //Constants definition
+.equ segind0 = 0xC0	//7-segment LED indicator's digits definitions
+.equ segind1 = 0xF9	//		 0
+.equ segind2 = 0xA4	//		 _
+.equ segind3 = 0xB0	//	5	|_|	1  <--6
+.equ segind4 = 0x99	//	4	|_|.7	2 
+.equ segind5 = 0x92	//
+.equ segind6 = 0x82	//		 3
+.equ segind7 = 0xF8	//
+.equ segind8 = 0x80	//
+.equ segind9 = 0x90	//
+.equ segindDash = 0b_1011_1111		//"-" sign
+.equ segindA = 0b_1000_1000		//"A"
+.equ segindB = 0b_1000_0011		//"b"
+.equ segindC = 0b_1100_0110		//"C"
+.equ segindD = 0b_1010_0001		//"d"
+.equ segindE = 0b_1000_0110		//"E"
+.equ segindF = 0b_1000_1110		//"F"
+
+.equ segindR = 0b_1010_1111		//"r"
+
+.equ presetLow = 0x39	//Here we can set a desired period of time for a precise delay (2 bytes long)
+.equ presetHigh = 0x01	//on 16 MHz and with no prescaler, there is approx. 62500 (0xF424) overflows per second (for 8-bit timer)
+						//and 0x7A12 on 8 MHz
+//150 Hz (0x01A1) works well for the red LED indicator
+//200 Hz (0x0139) for the green 4-digit indicator
+.equ owfPerSecond8BitLow = 0x24
+.equ owfPerSecond8BitHigh = 0xF4 //supra
 
 //Symbolic custom registers names
-.def currentPower				 =	R23
-.def overflowsCounterBlink		 =	R24
-.def overflowsCounterBlockAdj	 =	R25
+.def currentPower				 =	R17
+;.def overflowsCounterBlink		 =	R18
+.def overflowsCounterBlockAdj	 =	R18
+
+
+.def digitToDisp1 = R21			//1st digit to be displayed on the LED. Only hexadecimal digits are defined!
+.def digitToDisp2 = R22			//2nd digit to be displayed on the LED
+.def digitToDisp3 = R23			//3rd digit to be displayed on the LED
+.def digitToDisp4 = R20			//4th digit to be displayed on the LED
+.def overflowsCounterLow = R24		//Incrementing every time when overflow of the timer0 occurs
+.def overflowsCounterHigh = R25		//my my, there's too many overflows for a humble 8-bit register
+
 
 .def flagStorage = R19			//Custom flags storage
 								//And custom symbolic bit names, why not
 .equ blockAdj	=	0
 .equ ledState	=	1
+.equ achtung =						2	//USART: new data received
+.equ transmit =						3	//USART: a command sequence is recognized
+.equ timeToRefresh =				4	//LED digits should be refreshed
+.equ uartTXBufferOverflow =			5
+
+.equ control1 = 0x61			//"a"
+.equ control2 = 0x77			//"w"
+.equ control3 = 0x6B			//"k" (ASCII)
 
 .equ blink = 0x04				//0x04 = 55 ms on 8 MHz, prescaler=64, system divider=8 
 .equ buttonCutoff = 0x0B		//0x07 = 100 ms on 8 MHz, prescaler=64, system divider=8
 
-.equ owfPerSecond8BitHigh = 0x00
-.equ owfPerSecond8BitLow = 0x49		//8 MHz, prescaler=64, system divider=8 -> ~73 (0x0049) 8-bit overflows/sec
+.equ uartRAMStorageRXLength = 8	//well, a length of storage in RAM, dedicated for saving UART's received bytes
+.equ uartRAMStorageTXLength = 8	//same for bytes to transmit (255 bytes maximum!)
+
+;.equ owfPerSecond8BitHigh = 0x00
+;.equ owfPerSecond8BitLow = 0x49		//8 MHz, prescaler=64, system divider=8 -> ~73 (0x0049) 8-bit overflows/sec
 
 ;--------------------------------------------------------------------------------------------
 ;Macro definitions
@@ -57,13 +106,19 @@ POP R16			//Extract R16 value from stack
 .DSEG			//SRAM memory segment
 .ORG SRAM_START //start from the beginning
 
+uartRX:				.BYTE uartRAMStorageRXLength	//allocate space for read buffer...
+uartErrorsCounter:	.BYTE 1		//...and for errors counter
+
+uartTX:				.BYTE uartRAMStorageTXLength	//allocate space for write buffer
+uartTXRead:			.BYTE 1
+uartTXWrite:		.BYTE 1
 ;--------------------------------------------------------------------------------------------
 .CSEG
 //Reset and Interrupt Vectors table
 
 	.ORG 0x0000	;(RESET) 
 	RJMP Reset
-
+/*
 	.ORG INT0addr	;External Interrupt 0 (IRQ0 Handler)
 	RETI
 
@@ -85,6 +140,83 @@ POP R16			//Extract R16 value from stack
 	RETI
 	.ORG ADCCaddr	;ADC Conversion Complete
 	RETI
+*/
+
+	.ORG INT0addr	;(INT0) External Interrupt Request 0
+	RETI
+	.ORG INT1addr	;(INT1) External Interrupt Request 1
+	RETI
+	.ORG INT2addr	; External Interrupt Request 2
+	RETI
+	.ORG INT3addr	; External Interrupt Request 3
+	RETI
+
+	.ORG INT4addr	; External Interrupt Request 4
+	RJMP PinToggle
+
+	.ORG INT5addr	; External Interrupt Request 5
+	RETI
+	.ORG INT6addr	; External Interrupt Request 6
+	RETI
+	.ORG INT7addr	; External Interrupt Request 7
+	RETI
+	.ORG OC2addr	;(TIMER2 COMP) Timer/Counter2 Compare Match
+	RETI
+	.ORG OVF2addr	;(TIMER2 OVF) Timer/Counter2 Overflow
+	RETI
+	.ORG ICP1addr	;(TIMER1 CAPT) Timer/Counter1 Capture Event
+	RETI
+	.ORG OC1Aaddr	;(TIMER1 COMPA) Timer/Counter1 Compare Match A
+	RETI
+	.ORG OC1Baddr	;(TIMER1 COMPB) Timer/Counter1 Compare Match B
+	RETI
+	.ORG OVF1addr	;(TIMER1 OVF) Timer/Counter1 Overflow
+	RETI
+	.ORG OC0addr	;(TIMER0 COMP) Timer/Counter0 Compare Match
+	RETI
+
+	.ORG OVF0addr	;(TIMER0 OVF) Timer/Counter0 Overflow
+	RJMP Timer0Over
+
+	.ORG SPIaddr	;(SPI,STC) Serial Transfer Complete
+	RETI
+	.ORG URXC0addr	;(USART0,RXC) USART0, Rx Complete
+	RETI
+	.ORG UDRE0addr	;(USART0,UDRE) USART0 Data Register Empty
+	RETI
+	.ORG UTXC0addr	;(USART0,TXC) USART0, Tx Complete
+	RETI
+	.ORG ADCCaddr	;(ADC) ADC Conversion Complete
+	RETI
+	.ORG ERDYaddr	;(EE_RDY) EEPROM Ready
+	RETI
+	.ORG ACIaddr	;(ANA_COMP) Analog Comparator
+	RETI
+	.ORG OC1Caddr	; Timer/Counter1 Compare Match C
+	RETI
+	.ORG ICP3addr	; Timer/Counter3 Capture Event
+	RETI
+	.ORG OC3Aaddr	; Timer/Counter3 Compare Match A
+	RETI
+	.ORG OC3Baddr	; Timer/Counter3 Compare Match B
+	RETI
+	.ORG OC3Caddr	; Timer/Counter3 Compare Match C
+	RETI
+	.ORG OVF3addr	; Timer/Counter3 Overflow
+	RETI
+
+	.ORG URXC1addr	;(USART1,RXC) USART1, Rx Complete
+	RJMP U1_RXcomplete
+
+	.ORG UDRE1addr	;(USART1,UDRE) USART1 Data Register Empty
+	RJMP U1_DREmpty
+
+	.ORG UTXC1addr	;(USART1,TXC) USART1, Tx Complete
+	RETI
+	.ORG TWIaddr	;(TWI) 2-wire Serial Interface
+	RETI
+	.ORG SPMRaddr	;(SPM_RDY) Store Program Memory Ready
+	RETI
 
 .ORG INT_VECTORS_SIZE	;end of table
 
@@ -97,8 +229,7 @@ Timer0Over:
 PUSHSREG
  PUSH YL
   PUSH YH
-   PUSH R17
-   
+/*   
 INC overflowsCounterBlink
 
 SBRC flagStorage, ledState		//Skip next instruction if the LED is off
@@ -126,6 +257,26 @@ ledIsOn:
 		SBI PORTB, 2
 
 endBlink:
+*/
+
+ADIW overflowsCounterHigh:overflowsCounterLow, 1	//incrementing the whole word
+
+CPI overflowsCounterHigh, presetHigh	//if the higher register contains half of preset...
+BRLO notAFastPreset						//...AND...
+
+CPI overflowsCounterLow, presetLow		//...the lower register contains another half, then go to executing our payload
+BRLO notAFastPreset						//else - exit
+
+//this string executes once in a certain period
+	ORI flagStorage, (1<<timeToRefresh)	//Set timeToRefresh flag
+	LDI overflowsCounterLow, 0x00	//zeroing the overflows counter
+	LDI overflowsCounterHigh, 0x00
+	INC R17							//Circling from 0 to 3 periodically (fast), to determine which digit should be lit
+	CPI R17, 4						//compare with 4
+	BRNE notAFastPreset				//if not 4, then exit, else reset R17 to 0
+	LDI R17, 0						//reset to 0
+
+notAFastPreset:
 
 SBRS flagStorage, blockAdj			//If blockAdj==0 then skip
 RJMP endOfTimerInt
@@ -140,7 +291,6 @@ RJMP endOfTimerInt
 
 endOfTimerInt:
 
-   POP R17
   POP YH
  POP YL
 POPSREG
@@ -202,6 +352,110 @@ buttonReleased:
 
 ;-------------------
 
+U1_DREmpty:	//USART 1 Data Register Empty Interrupt
+
+PUSH R18
+ PUSH YH
+  PUSH YL
+   PUSHSREG	//Store both R16 and SREG in stack
+
+LDS R16, uartTXRead		//Get a read and write pointers
+LDS R18, uartTXWrite
+
+SBRC flagStorage, uartTXBufferOverflow	//If an overflow has occured...
+RJMP unreadData							//..then go to data transfer
+
+CP R16, R18			//If the pointers are equal, then all data has been read and sent
+BRNE unreadData
+
+	//Buffer is empty
+	UIN R18, UCSR1B
+	ANDI R18, ~(1<<UDRIE1)	//Forbid this interrupt
+	UOUT UCSR1B, R18
+	RJMP exitTXIntTrue		//Exit
+
+unreadData:
+
+LDI YL, low(uartTX)		//Get a start address
+LDI YH, high(uartTX)
+ADD YL, R16
+CLR R18
+ADC YH, R18				//Add with carry the read pointer
+
+LD R18, Y				//Extract a value...
+UOUT UDR1, R18			//...and send it through UART
+
+ANDI flagStorage, ~(1<<uartTXBufferOverflow)	//Clear TX Overflow flag
+INC R16					//Increment the read pointer
+CPI R16, uartRAMStorageTXLength	//Compare it with buffer length
+BRLO exitTXInt
+	CLR R16				//Clear if needed
+exitTXInt:
+
+STS uartTXRead, R16		//Store the read pointer
+
+exitTXIntTrue:
+
+   POPSREG		//Extract SREG and R16 from stack
+  POP YL
+ POP YH
+POP R18
+
+RETI
+
+;-------------------
+
+U1_RXcomplete:	//USART 1 Receive Complete interrupt
+
+PUSH R18
+ PUSH YH			//Saving in stack registers to be used
+  PUSH YL
+   PUSHSREG	//Store both R16 and SREG in stack
+
+CLR R18
+UIN R16, UCSR1A		//Read error flags
+SBRC R16, 2			//Skip if no error
+	SUBI R18, (-1)	//Else increment R18
+SBRC R16, 3			//Skip...
+	SUBI R18, (-1)	//...Increment
+SBRC R16, 4			//Skip...
+	SUBI R18, (-1)	//...Increment
+
+LDI YL, low(uartErrorsCounter)	//Load Y pair with address of the Errors SRAM storage
+LDI YH, high(uartErrorsCounter)
+LD R16, Y						//Read error count from SRAM
+ADD R16, R18					//Apply new errors to it...
+ST Y, R16						//And store back
+
+UIN R16, UDR1	//Read received data
+CPI R18, 0x00	//If there are read errors...
+BREQ errorFF
+	LDI R16, 0xFF	//Then write 0xFF instead of data
+errorFF:
+ST X+, R16		//Store data in SRAM and post-increment
+
+LDI YL, low(uartRX)	//Load Y pair with start address of the SRAM storage
+LDI YH, high(uartRX)
+ADIW YH:YL, (uartRAMStorageRXLength-1)	//add (storage_size - 1) to Y
+CP YL, XL
+CPC YH, XH			//If X pointer reached last allocated cell in the storage...
+BRGE exitInt2		//...then reset it to the beginning
+	LDI XL, low(uartRX)	//load X pointer with address of SRAM storage
+	LDI XH, high(uartRX)
+
+exitInt2:
+
+ORI flagStorage, (1<<achtung)	//Achtung! Some new data received!
+
+   POPSREG		//Extract SREG and R16 from stack
+  POP YL		//Extracting saved values from stack
+ POP YH
+POP R18
+
+RETI
+
+;-------------------
+
 //End of Interrupts Handler//
 
 ;--------------------------------------------------------------------------------------------
@@ -209,6 +463,10 @@ buttonReleased:
 
 fixedLevels:	.db 0x4D, 0x7F, 0xB3, 0xDA, 0xFF, 0x00
 
+decAddrTable: .dw disp0, disp1, disp2, disp3, disp4, \
+		disp5, disp6, disp7, disp8, disp9, \
+		dispA, dispB, dispC, dispD, dispE, dispF, \
+		dispR, dispDash	//Adresses of the labels, stored in a certain place (decAddrTable) in program memory
 ;--------------------------------------------------------------------------------------------
 Reset:
 
@@ -236,24 +494,77 @@ Flush:		ST 	Z+, R16				; Flush byte and increment
 //Thanks for code to DI HALT, Testicq and all fellow comrades from easyelectronics.ru
 
 //Stack initialization
-LDI R16, RAMEND
-UOUT SPL, R16
+;LDI R16, RAMEND
+;UOUT SPL, R16
+LDI R16, Low(RAMEND)
+OUT SPL, R16
+LDI R16, High(RAMEND)
+OUT SPH, R16
 
 //------------------------------------
 
 //Timer initialization
-LDI R16, 0b_0000_0010	//interrupts
-UOUT TIMSK0, R16			//set TOIE0 in TIMSK0 register (overflow enabled)
 
 	//Timer2
-	LDI R16, 0b_0000_0100	//PWM pin disabled by default
+	UIN R16, DDRB
+	ANDI R16, ~(1 << PB7)	//PWM pin disabled by default
 	UOUT DDRB, R16
 
 	LDI R16, 0b_1000_0011	//Fast PWM mode
-	UOUT TCCR0A, R16
+	UOUT TCCR2, R16
 
 	LDI R16, 0b_0000_0011	//clock/64
 	UOUT TCCR0B, R16
+
+
+//Timer0 Initialization
+LDI R16, 0b_0000_0001	//set CS00 bit in TCCR0 register
+OUT TCCR0, R16			//now using system clock for Timer0 without prescaler
+CLR R16
+ANDI R16, (1 << TOIE0 | 1 << TOIE2)	//set CS00 bit in TCCR0 register
+OUT TIMSK, R16			//set TOIE0 in TIMSK register
+//now we have the overflow interrupt enabled for timer0
+
+//------------------------------------
+
+//USART1 Initialization
+
+// Set baud rate (f osc = 16 MHz)
+// 2400 baud -> 0x01A0
+// 9600 baud -> 0x0067
+// 1Mbaud -> 0x0000
+// For 8 MHz, to achieve the same speed grades, U2X bit should be enabled
+LDI R16, 0x00
+UOUT UBRR1H, R16
+LDI R16, 0x00
+UOUT UBRR1L, R16
+
+// 7 - (RXC1) USART Receive Complete					(r/o)
+// 6 - (TXC1) USART Transmit Complete (clearing by writing 1)
+// 5 - (UDRE1) USART Data Register Empty				(r/o)
+// 4 - (FE1) Frame Error (must be set to 0)			(r/o)
+// 3 - (DOR1) Data Overrun (must be set to 0)		(r/o)
+// 2 - (UPE1) USART Parity Error (must be set to 0)	(r/o)
+// 1 - (U2X1) Double the USART Transmission Speed
+// 0 - (MPCM1) Multi-Processor Communication Mode
+LDI R16, 0b_0100_0000
+UOUT UCSR1A, R16
+
+// 7 - (RXCIE1) RX Complete Interrupt Enable
+// 6 - (TXCIE1) TX Complete Interrupt Enable
+// 5 - (UDRIE1) USART Data Register Empty Interrupt Enable
+// 4 - (RXEN1) Receiver Enable
+// 3 - (TXEN1) Transmitter Enable
+// 2 - (UCSZ12) Character Size (combined with the UCSZn1:0 bit in UCSRC)
+// 1 - (RXB81) Receive Data Bit 8 (for nine data bits only)	(r/o)
+// 0 - (TXB81) Transmit Data Bit 8 (for nine data bits only)
+// TX Complete' and 'UDR Empty' interrupts enable, enable receiver and transmitter, no ninth bit:
+LDI R16, 0b_1011_1000
+UOUT UCSR1B, R16
+
+// Set frame format: asynchronous operation, no parity, 8 data, 1 stop bit
+LDI R16, 0b_0000_0110
+UOUT UCSR1C, R16
 
 //------------------------------------
 //external interrupt init
@@ -266,9 +577,16 @@ UOUT PCMSK, R16
 
 //------------------------------------
 //gpio init
-
+/*
 LDI R16, 0b_0000_0100		//tri-stated inputs, low level on outputs
 UOUT PORTB, R16
+*/
+LDI R16, 0b_0000_0100		//tri-stated inputs, low level on outputs
+UOUT PORTF, R16
+
+LDI R16, 0xFF
+OUT DDRC, R16			//write 1-s into each port C...
+OUT DDRA, R16			//...and port A direction registers
 
 //------------------------------------
 
@@ -283,7 +601,7 @@ SEI			//interrupts enabled globally
 //Main Routine//
 Start:
 
-UIN R16, PINB
+UIN R16, PINF
 ANDI R16, (1<<1)
 BRNE label1
 
@@ -306,8 +624,8 @@ LDI ZL, low(fixedLevels)
 LDI ZH, high(fixedLevels)
 MOV R16, currentPower
 ADD ZL, R16
-CLR R17
-ADC ZH, R17
+CLR R16
+ADC ZH, R16
 LPM R16, Z
 
 UOUT OCR0A, R16
